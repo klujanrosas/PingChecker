27bac02e45c979fc0e0d25086d2b403e306cc56d
Kenneth Ubaldo Lujan Rosas <elgenio.03@gmail.com>
2016-12-31 14:43:01 -0500

    Added embedded res

--- PingChecker/RadForm1.cs
+++ PingChecker/RadForm1.cs
@@ -3,8 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
 using System.Linq;
 using System.Net.NetworkInformation;
+using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,11 +18,41 @@ namespace PingChecker
 {
     public partial class RadForm1 : Telerik.WinControls.UI.RadForm
     {
+        PrivateFontCollection private_fonts = new PrivateFontCollection();
         public RadForm1()
         {
             InitializeComponent();
 
             ThemeResolutionService.ApplicationThemeName = "VisualStudio2012Dark";
+            // specify embedded resource name
+            string resource = "PingChecker.Hoonburguer.ttf";
+
+            // receive resource stream
+            Stream fontStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resource);
+
+            // create an unsafe memory block for the font data
+            System.IntPtr data = Marshal.AllocCoTaskMem((int)fontStream.Length);
+
+            // create a buffer to read in to
+            byte[] fontdata = new byte[fontStream.Length];
+
+            // read the font data from the resource
+            fontStream.Read(fontdata, 0, (int)fontStream.Length);
+
+            // copy the bytes to the unsafe memory block
+            Marshal.Copy(fontdata, 0, data, (int)fontStream.Length);
+
+            // pass the font to the font collection
+            private_fonts.AddMemoryFont(data, (int)fontStream.Length);
+
+            // close the resource stream
+            fontStream.Close();
+
+            // free up the unsafe memory
+            Marshal.FreeCoTaskMem(data);
+
+            radLabel1.UseCompatibleTextRendering = true;
+            radLabel1.Font = new Font(private_fonts.Families[0], 20);
         }
 
         private async void radButton1_Click(object sender, EventArgs e)
