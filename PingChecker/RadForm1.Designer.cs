9add6edd7cf20c1776239d840cc051e540f11771
Kenneth Ubaldo Lujan Rosas <elgenio.03@gmail.com>
2016-12-31 14:24:00 -0500

    Initial Commit

---
+++ PingChecker/RadForm1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace PingChecker
+{
+    partial class RadForm1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.visualStudio2012DarkTheme1 = new Telerik.WinControls.Themes.VisualStudio2012DarkTheme();
+            this.radButton1 = new Telerik.WinControls.UI.RadButton();
+            this.radButton2 = new Telerik.WinControls.UI.RadButton();
+            this.radLabel1 = new Telerik.WinControls.UI.RadLabel();
+            this.radLabel2 = new Telerik.WinControls.UI.RadLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // radButton1
+            // 
+            this.radButton1.Location = new System.Drawing.Point(12, 43);
+            this.radButton1.Name = "radButton1";
+            this.radButton1.Size = new System.Drawing.Size(110, 24);
+            this.radButton1.TabIndex = 0;
+            this.radButton1.Text = "PING LOL LAN";
+            this.radButton1.ThemeName = "VisualStudio2012Dark";
+            this.radButton1.Click += new System.EventHandler(this.radButton1_Click);
+            // 
+            // radButton2
+            // 
+            this.radButton2.Location = new System.Drawing.Point(12, 110);
+            this.radButton2.Name = "radButton2";
+            this.radButton2.Size = new System.Drawing.Size(110, 24);
+            this.radButton2.TabIndex = 1;
+            this.radButton2.Text = "PING iad.valve.net";
+            this.radButton2.ThemeName = "VisualStudio2012Dark";
+            this.radButton2.Click += new System.EventHandler(this.radButton2_Click);
+            // 
+            // radLabel1
+            // 
+            this.radLabel1.Font = new System.Drawing.Font("1Hoonburger R", 20F);
+            this.radLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(172)))), ((int)(((byte)(238)))));
+            this.radLabel1.Location = new System.Drawing.Point(191, 43);
+            this.radLabel1.Name = "radLabel1";
+            this.radLabel1.Size = new System.Drawing.Size(62, 32);
+            this.radLabel1.TabIndex = 2;
+            this.radLabel1.Text = "IDLE";
+            this.radLabel1.ThemeName = "VisualStudio2012Dark";
+            // 
+            // radLabel2
+            // 
+            this.radLabel2.Font = new System.Drawing.Font("1Hoonburger R", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.radLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(85)))), ((int)(((byte)(172)))), ((int)(((byte)(238)))));
+            this.radLabel2.Location = new System.Drawing.Point(190, 110);
+            this.radLabel2.Name = "radLabel2";
+            this.radLabel2.Size = new System.Drawing.Size(63, 32);
+            this.radLabel2.TabIndex = 3;
+            this.radLabel2.Text = "IDLE";
+            this.radLabel2.ThemeName = "VisualStudio2012Dark";
+            // 
+            // RadForm1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(342, 210);
+            this.Controls.Add(this.radLabel2);
+            this.Controls.Add(this.radLabel1);
+            this.Controls.Add(this.radButton2);
+            this.Controls.Add(this.radButton1);
+            this.Name = "RadForm1";
+            // 
+            // 
+            // 
+            this.RootElement.ApplyShapeToControl = true;
+            this.Text = "PingChecker";
+            this.ThemeName = "VisualStudio2012Dark";
+            ((System.ComponentModel.ISupportInitialize)(this.radButton1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radButton2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.radLabel2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Telerik.WinControls.Themes.VisualStudio2012DarkTheme visualStudio2012DarkTheme1;
+        private Telerik.WinControls.UI.RadButton radButton1;
+        private Telerik.WinControls.UI.RadButton radButton2;
+        private Telerik.WinControls.UI.RadLabel radLabel1;
+        private Telerik.WinControls.UI.RadLabel radLabel2;
+    }
+}
---
+++ PingChecker/RadForm1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Telerik.WinControls;
+
+namespace PingChecker
+{
+    public partial class RadForm1 : Telerik.WinControls.UI.RadForm
+    {
+        public RadForm1()
+        {
+            InitializeComponent();
+
+            ThemeResolutionService.ApplicationThemeName = "VisualStudio2012Dark";
+        }
+
+        private async void radButton1_Click(object sender, EventArgs e)
+        {
+            radLabel1.Text = "Pinging...";
+            string result = await Task.Run(() => GetAveragegMs("104.160.136.3"));
+            radLabel1.Text = $"{result}ms";
+        }
+        private async void radButton2_Click(object sender, EventArgs e)
+        {
+            radLabel2.Text = "Pinging...";
+            string result = await Task.Run(() => GetAveragegMs("iad.valve.net"));
+            radLabel2.Text = $"{result}ms";
+        }
+
+        public string GetAveragegMs(string address) 
+        {
+            int avg = 0;
+            using (Ping ping = new Ping())
+            {
+                try
+                {
+                   
+                        for (int i = 0; i < 10; i++)
+                        {
+                            PingReply reply = ping.Send(address);
+
+                            if (reply.Status == IPStatus.Success)
+                            {
+                                avg += (int)reply.RoundtripTime;
+                            }
+                        }
+
+
+                    return (avg / 10).ToString();
+
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+            }
+        }
+
+
+    }
+}
